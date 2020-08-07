5bf49643e3bfc4b8023b04c837b64c15df5246e4
ZengPai <1023479515@qq.com>
2020-08-07 14:33:54 +0800

    安装了ElementUI

---
+++ src/components/Comments.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <H1>
+      This is a blank page
+    </H1>
+
+    <div>
+      <div style="margin: 20px 0;"></div>
+      <el-input
+        type="textarea"
+        placeholder="请输入内容"
+        v-model="textarea"
+        maxlength="30"
+        show-word-limit
+      >
+      </el-input>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'comments',
+  data () {
+    return {
+      text: '',
+      textarea: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '../components/HelloWorld'
+import Comments from '../components/Comments'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/comments',
+      name: 'Comments',
+      component: Comments
     }
   ]
 })
